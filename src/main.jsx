ca8a5a599a3eba98866c0da6a1fbaae6f2e56369
reutrose <reutroz@gmail.com>
2025-01-26 10:12:16 +0200

    Restoring strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./components/Theme.jsx";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-	<ThemeProvider>
-		<App />
-	</ThemeProvider>
+	<StrictMode>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</StrictMode>
 );
